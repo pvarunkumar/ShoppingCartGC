2e8406269b9138eef53fb1ebe1172ac2ea3951c9
Venkata Patchipulusu <ArunKumar@venkatas-mbp.home>
2017-10-25 15:20:26 +0100

    Step2 commit

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -9,6 +9,19 @@ object ShoppingCart {
     for (scItem <- scItems)
       scTotal += scItemPrice(scItem)
 
-    scTotal
+    scTotal - scDiscount(scItems)
+
+  }
+
+
+  def scDiscount(scItems: Array[String]): BigDecimal = {
+
+    var discTotal: BigDecimal = 0
+    val AppleCount = scItems.count(itm => itm == "Apple")
+    val OrangeCount = scItems.count(itm => itm == "Orange")
+
+    discTotal = BigDecimal.decimal((AppleCount / 2) * scItemPrice("Apple")) + ((OrangeCount / 3) * scItemPrice("Orange"))
+
+    discTotal
   }
 }
